d7fe43dd7a69826f3fd367e18caae6c7a8438233
alegrandoi <zzalejandro_3@hotmail.com>
2020-02-27 13:29:03 +0000

    final version l04

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const coo1 = require("cool-ascii-faces");
+const express = require ("express");
+var app =express();
+app.listen(80);
+
+app.get("/cool",(resquest,response) =>{
+	response.send("<html>cool()</html>");
+});
 
 console.log(coo1());
+
