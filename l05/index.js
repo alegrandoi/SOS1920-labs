ec63437013f5c151ea2663773fa9c169ff4a3c08
alegrandoi <zzalejandro_3@hotmail.com>
2020-03-20 10:08:10 +0000

    empieza la clase

--- l05/index.js
+++ l05/index.js
@@ -1,6 +1,5 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 
 var app = express();
 
@@ -8,31 +7,28 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 80;
 
-app.use("/",express.static("./public"));
-
-app.get("/cool",(request,response) => {
-	response.send("<html>"+cool()+"</html>");
-});
-
 var contacts = [
-	{
+	{ 
 		name: "peter",
-		phone: 12345
+		phone: 123456	
 	},
-	{
+	{ 
 		name: "pablo",
-		phone: 1234
+		phone: 789456	
 	}
 ];
+
 const BASE_API_URL = "/api/v1";
 
-app.get(BASE_API_URL + "/contacts", (request,response) => {
-	response.send(JSON.stringify(contacts,null,2));
+
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
 });
 
-app.post(BASE_API_URL + "/contacts", (request,response) => {
-	contacts.push(request.body);
-	response.sendStatus(201,"CREATED");
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	contacts.push(req.body); 
+	res.sendStatus(201,"CREATED");
 });
 
 app.listen(port, () => {
